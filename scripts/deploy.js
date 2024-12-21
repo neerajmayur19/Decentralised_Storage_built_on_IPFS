4a0f9740836522484ae16cf4f026c6faac540d11
neerajmayur19 <neeraj.mayur.999@gmail.com>
2024-12-21 18:33:27 +0530

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Upload = await hre.ethers.getContractFactory("Upload");
+  const upload = await Upload.deploy();
+
+  await upload.deployed();
+  console.log("Library Deployed to:", upload.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+//Re-deploy the contract everytime you close the TERMINAL. REMEMBER.
